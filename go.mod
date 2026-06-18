a7408931c7f4fb249d84943ff3fcbefb129b110e
Max Kaiser <max@kaiser.family>
2026-06-18 15:01:44 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maxkaiser11/monkey
+
+go 1.25.6
---
+++ token/token.go
@@ -0,0 +1,47 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers
+	IDENT = "IDENT" // add, foobar
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+
+	FUNCION = "FUNCTION"
+	LET     = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
